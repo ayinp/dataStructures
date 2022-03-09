3f4a87a51571e1eb346467d1a62b3c9170a87989
Ayi_n <sourlemon2270@gmail.com>
2022-03-09 09:01:44 -0500

    all bebber now

--- priorityq/main.cpp
+++ priorityq/main.cpp
@@ -8,265 +8,121 @@
 using namespace std;
 
 template <typename T>
-class Stack{
+class PriorityQueue{
 public:
-    T* values;
-    int sz = 4;
-    int numStored = 0;
+    vector<T> things = {};
 public:
-    Stack();
-    ~Stack();
-    void push(T value);
-    //pushes on to value
-    T pop();
-    //pop off top
-    T top();
-    //returns top value without removing it
+    PriorityQueue();
+    ~PriorityQueue();
+    void insert(T value);
+    T remove();
     int size();
-    //size
     bool isEmpty();
-    //checks if empty
 };
 
 template<typename T>
-Stack<T>::Stack()
+PriorityQueue<T>::PriorityQueue()
 {
-    values = new T[sz];
 }
 
 template<typename T>
-Stack<T>::~Stack()
+PriorityQueue<T>::~PriorityQueue()
 {
-    delete [] values;
 }
 
 template<typename T>
-void Stack<T>::push(T value)
+void PriorityQueue<T>::insert(T value)
 {
-    if(numStored >= sz){
-        sz*=2;
-        T* newValues = new T[sz];
-        for(int i = 0; i < numStored; i++){
-            newValues[i] = values[i];
-        }
-        delete [] values;
-        values = newValues;
-        values[numStored] = value;
-        numStored++;
-    }
-    else{
-        values[numStored] = value;
-        numStored++;
-    }
-
 }
 
 template<typename T>
-T Stack<T>::pop()
+T PriorityQueue<T>::remove()
 {
-    int poped = values[numStored-1];
-    //need to check if is empty so not looking at -1 index
-    numStored -= 1;
-    T* newValues = new T[sz];
-    for(int i = 0; i < numStored; i++){
-        newValues[i] = values[i];
-    }
-    delete [] values;
-    values = newValues;
-    return poped;
 }
 
 template<typename T>
-T Stack<T>::top()
+int PriorityQueue<T>::size()
 {
-    return values[numStored-1];
 }
 
 template<typename T>
-int Stack<T>::size()
+bool PriorityQueue<T>::isEmpty()
 {
-    return numStored;
 }
 
-template<typename T>
-bool Stack<T>::isEmpty()
-{
-    return numStored == 0;
-}
-
-
-TEST(TestStack, Push1TestSizeAndValues) {
-    Stack<int> s;
-    s.push(1);
-    ASSERT_EQ(s.size(), 1);
-}
-
-TEST(TestStack, Push3TestSizeAndValues) {
-    Stack<int> s;
-    s.push(4);
-    s.push(5);
-    s.push(6);
-    ASSERT_EQ(s.size(), 3);
-}
-
-TEST(TestStack, Push50TestSizeAndValues) {
-    Stack<int> s;
-    for(int i = 0; i < 50; i++){
-        s.push(i);
-    }
-    ASSERT_EQ(s.size(), 50);
-    for(int i = 0; i < 50; i++){
-        ASSERT_EQ(s.pop(), 49-i);
-    }
-}
-
-TEST(TestStack, PopTest1) {
-    Stack<int> s;
-    s.push(6);
-    s.push(7);
-    s.push(8);
-    ASSERT_EQ(s.size(), 3);
-    ASSERT_EQ(s.top(), 8);
-    s.pop();
-    ASSERT_EQ(s.size(), 2);
-    ASSERT_EQ(s.top(), 7);
-}
-
-TEST(TestStack, PopTest2) {
-    Stack<int> s;
-    s.push(6);
-    s.push(7);
-    s.push(8);
-    ASSERT_EQ(s.size(), 3);
-    ASSERT_EQ(s.pop(), 8);
-    ASSERT_EQ(s.size(), 2);
-}
-
-TEST(TestStack, TopTest) {
-    Stack<int> s;
-    s.push(6);
-    s.push(7);
-    s.push(8);
-    ASSERT_EQ(s.top(), 8);
-    s.push(9);
-    ASSERT_EQ(s.top(), 9);
-    s.pop();
-    ASSERT_EQ(s.top(), 8);
-}
-
-TEST(TestStack, SizeTestEmpty) {
-    Stack<int> s;
+TEST(TestPQ, SizeTestEmpty) {
+    PriorityQueue<int> s;
     ASSERT_EQ(s.size(), 0);
 }
 
-TEST(TestStack, SizeTest1) {
-    Stack<int> s;
-    s.push(6);
+TEST(TestPQ, SizeTest1) {
+    PriorityQueue<int> s;
+    s.insert(6);
     ASSERT_EQ(s.size(), 1);
 }
 
-TEST(TestStack, SizeTest2) {
-    Stack<int> s;
-    s.push(6);
-    s.push(7);
-    s.push(8);
+TEST(TestPQ, SizeTest2) {
+    PriorityQueue<int> s;
+    s.insert(6);
+    s.insert(7);
+    s.insert(8);
     ASSERT_EQ(s.size(), 3);
 }
 
-TEST(TestStack, EmptyTest) {
-    Stack<int> s;
+TEST(TestPQ, EmptyTest) {
+    PriorityQueue<int> s;
     ASSERT_TRUE(s.isEmpty());
-    s.push(6);
+    s.insert(6);
     ASSERT_FALSE(s.isEmpty());
-    s.push(7);
-    s.push(8);
+    s.insert(7);
+    s.insert(8);
     ASSERT_FALSE(s.isEmpty());
-    s.pop();
-    s.pop();
-    s.pop();
+    s.remove();
+    s.remove();
+    s.remove();
     ASSERT_TRUE(s.isEmpty());
 }
 
-//template <typename T>
-//class myVector {
-//private:
-//    vector<T> values;
-//public:
-//    myVector();
-//    void push_back(T v);
-//    T get(int index);
-//    int size();
-//};
+TEST(TestPQ, TestHeapProperty) {
+    PriorityQueue<int> s;
+    for(int i = 0; i < 50; i++){
+        s.insert(i);
+    }
+    ASSERT_EQ(s.size(), 50);
+    int last = s.remove();
+    for(int i = 0; i < 49; i++){
+        ASSERT_LE(s.remove(), last);
+    }
+}
 
-//template <typename T>
-//myVector<T>::myVector()
-//{
-//}
+TEST(TestPQ, PopTest1) {
+    PriorityQueue<int> s;
+    s.insert(6);
+    s.insert(7);
+    s.insert(8);
+    ASSERT_EQ(s.size(), 3);
+    ASSERT_EQ(s.remove(), 8);
+    ASSERT_EQ(s.size(), 2);
+}
 
-//template <typename T>
-//void myVector<T>::push_back(T v)
-//{
-//    values.push_back(v);
-//}
 
-//template <typename T>
-//T myVector<T>::get(int index)
-//{
-//    return values[index];
-//}
 
-//template <typename T>
-//int myVector<T>::size()
-//{
-//    return values.size();
-//}
+TEST(TestPQ, TopTest) {
+    PriorityQueue<int> s;
+    s.insert(6);
+    s.insert(7);
+    s.insert(8);
+    ASSERT_EQ(s.remove(), 8); // 8 is gone
+    s.insert(9); // 9 is here
+    ASSERT_EQ(s.remove(), 9); // 9 is gone
+    s.insert(5); // 5 is here
+    ASSERT_EQ(s.remove(), 7); // 7 is gone
+    ASSERT_EQ(s.remove(), 6); // 6 is gone
+    ASSERT_EQ(s.remove(), 5); // 5 is gone
+    ASSERT_EQ(s.size(), 0); // should be empty
+}
 
-//TEST(TestVector, NewVectorIsEmpty) {
-//    myVector<double> v;
-////    ASSERT_TRUE(v.size() == 0);
-//    ASSERT_EQ(v.size(), 0);
-//}
 
-//TEST(TestVector, SizeIsOneAfterOnePushBack) {
-//    myVector<double> v;
-//    v.push_back(37);
-//    ASSERT_EQ(v.size(), 1);
-//}
-
-//TEST(TestVector, PushTwoValuesAndCheckSize){
-//    myVector<double> v;
-//    v.push_back(1);
-//    v.push_back(49);
-//    ASSERT_EQ(v.get(0), 1);
-//    ASSERT_EQ(v.get(1), 49);
-//    ASSERT_EQ(v.size(), 2);
-//}
-
-//TEST(TestVector, CanRetrievePushedValue) {
-//    myVector<double> v;
-//    v.push_back(37);
-//    ASSERT_EQ(v.get(0), 37);
-//}
-
-//TEST(TestVector, Push50Things){
-//    myVector<double> v;
-//    for(int i = 0; i < 50; i++){
-//        v.push_back(i);
-//        ASSERT_EQ(v.get(i), i);
-//    }
-//    ASSERT_EQ(v.size(), 50);
-//}
-
-//TEST(TestVector, Push50ThingsAgain){
-//    myVector<double> v;
-//    for(int i = 0; i < 50; i++){
-//        v.push_back(i);
-//    }
-//    for(int i = 0; i < 50; i++){
-//        ASSERT_EQ(v.get(i), i);
-//    }
-//    ASSERT_EQ(v.size(), 50);
-//}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
